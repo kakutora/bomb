d51fb4c74987055aef8b454bfc36dbdcfad6a3c0
kakutora <87274851+kakutora@users.noreply.github.com>
2023-06-07 16:59:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const path = require('path');
 const http = require("http");
 const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+// ドキュメントルートの設定
+
+app.use(express.static(path.join(__dirname, '/views')));
+
+/*
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
+*/
+
 app.use("/js", express.static(__dirname + "/js/"));
 app.use("/img", express.static(__dirname + "/img/"));
+
 app.use(
     "/io",
     express.static(__dirname + "/node_modules/socket.io/client-dist/")
 );
+
 server.listen(3000, () => {
     console.log("Server started on port 3000");
 });
--- js/canvas.js
+++ js/canvas.js
@@ -1,84 +1,107 @@
-//マップの作成（さくせい）
-let map = [
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1],
-    [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1],
-    [0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1],
-    [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1],
-    [1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
-    [0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1],
-    [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0],
-    [1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0],
-];
+let futsu_ga_ichiban = {
+    mapName: "testMap",
+    map: [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+    moveAllow: [0, 3, 4, 5, 7],//指定したマップチップの衝突判定
+    MAP_IMG_PATH: "/img/map3.png",//マップチップ画像
+    MAP_WIDTH: 15,//マスの数（幅）
+    MAP_HEIGHT: 15,//マスの数（高さ）
+    TILESIZE: 32,//マップチップのサイズ(px)
+    TILECOLUMN: 4,//マップチップ画像（列）
+    TILEROW: 2,//マップチップ画像（行）
+    firstPlayerPos: {
+        p1: {
+            x: 32,
+            y: 32
+        },
+        p2: {
+            x: 32,
+            y: 32
+        },
+        p3: {
+            x: 32,
+            y: 32
+        },
+        p4: {
+            x: 32,
+            y: 32
+        },
+    }
+};
+
+const map1 = JSON.parse(JSON.stringify(futsu_ga_ichiban));
 
 //----------------------------------------------------------------
 
 const SMOOTH = 0;//補完処理
 
-const INTERVAL = 15;//フレームレート設定
+const img = {//画像パス
+    chr: "/img/chr.png"
+};
 
-const VTL_WIDTH = 512;//マップの幅の総ピクセル数
-const VTL_HEIGHT = 512;//マップの高さの総ピクセル数
-
-const MAP_WIDTH = 16;//マップの幅（マス）
-const MAP_HEIGHT = 16;//マップの高さ（マス）
-
-const TILESIZE = 32;
+let mapImg;//マップチップ画像
+let chrImg;//マップチップ画像
 
 let vScreen;//仮想画面
 let rScreen;//実画面
+
 let rWidth;//実画面の幅
 let rHeight;//実画面の高さ
 
-let mapImg;//マップチップ画像
-let ricoImg;//マップチップ画像
+let defaultMoveSpeed = 8;
 
-const img = {//画像パス
-    map: "/img/map.png",
-    rico: "/img/rico.png"
-};
-
-const loadImage = () => {
-    mapImg = new Image();
-    mapImg.src = img.map;
-
-    ricoImg = new Image();
-    ricoImg.src = img.rico;
-};
-
-let playerX = 0;
-let playerY = 0;
+let playerX = 32;
+let playerY = 32;
 let playerMove = 0;
 
-let key = new Object();
-key.up = false;
-key.down = false;
-key.right = false;
-key.left = false;
-key.push = '';
+let key = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+};
 
 //----------------------------------------------------
 
 window.addEventListener('load', () => {
-    loadImage();
-    vScreen = document.createElement('canvas');
-    vScreen.width = VTL_WIDTH;
-    vScreen.height = VTL_HEIGHT;
-
-    canvasSize();
-    window.addEventListener("resize", () => { canvasSize(); });
-
-    mainLoop();
+    firstProcesses(map1);
+    mainLoop(map1);
 });
 
-const canvasSize = () => {
+const firstProcesses = (map) => {
+    loadImage(map);
+    vScreen = document.createElement('canvas');
+    vScreen.width = (map.TILESIZE * map.MAP_WIDTH);
+    vScreen.height = (map.TILESIZE * map.MAP_HEIGHT);
+
+    canvasSize(map);
+    window.addEventListener("resize", () => { canvasSize(map); });
+};
+
+const loadImage = (map) => {
+    mapImg = new Image();
+    mapImg.src = map.MAP_IMG_PATH;
+
+    chrImg = new Image();
+    chrImg.src = img.chr;
+};
+
+const canvasSize = (map) => {
     const c = document.querySelector('#canvas');
     c.width = window.innerWidth / 1.15;
     c.height = window.innerHeight / 1.15;
@@ -89,20 +112,22 @@ const canvasSize = () => {
     rWidth = c.width;
     rHeight = c.height;
 
-    if (rWidth / VTL_WIDTH < rHeight / VTL_HEIGHT) {
-        rHeight = rWidth * VTL_HEIGHT / VTL_WIDTH;
+    if (rWidth / (map.TILESIZE * map.MAP_WIDTH) < rHeight / (map.TILESIZE * map.MAP_HEIGHT)) {
+        rHeight = rWidth * (map.TILESIZE * map.MAP_HEIGHT) / (map.TILESIZE * map.MAP_WIDTH);
     } else {
-        rWidth = rHeight * VTL_WIDTH / VTL_HEIGHT;
+        rWidth = rHeight * (map.TILESIZE * map.MAP_WIDTH) / (map.TILESIZE * map.MAP_HEIGHT);
     }
 };
 
-const mainLoop = () => {
+const mainLoop = (map) => {
     realPaint();
     addEventListener("keydown", keydownfunc, false);
     addEventListener("keyup", keyupfunc, false);
-    keyInput();
-    KeyOutput();
-    requestAnimationFrame(mainLoop);
+    keyInput(map);
+    KeyOutput(defaultMoveSpeed);
+    requestAnimationFrame(() => {
+        mainLoop(map);
+    });
 };
 
 const realPaint = () => {
@@ -116,76 +141,63 @@ const realPaint = () => {
 
 const vtrPaint = () => {
     const ctx = vScreen.getContext("2d");
-    paintField(ctx);
+    paintAll(ctx);
 };
 
-const paintField = (ctx) => {
-    for (let dy = 0; dy < map.length; dy++) {
-        for (let dx = 0; dx < map[dy].length; dx++) {
-            ctx.drawImage(
-                mapImg,
-                TILESIZE * map[dy][dx], 0, TILESIZE, TILESIZE,
-                TILESIZE * dx, TILESIZE * dy, TILESIZE, TILESIZE
-            );
+
+const paintAll = (ctx) => {
+    paintField(ctx, map1);
+    ctx.drawImage(chrImg, playerX, playerY);
+};
+
+const paintField = (ctx, map) => {
+    for (let dy = 0; dy < map.map.length; dy++) {
+        for (let dx = 0; dx < map.map[dy].length; dx++) {
+            paintMapTile(ctx, mapImg, map.TILESIZE, map.TILECOLUMN, dy, dx, map.map[dy][dx]);
         }
     }
-
-    ctx.drawImage(ricoImg, playerX, playerY);
 };
 
-const keyInput = () => {
-    //方向キーが押されている場合（ばあい）は、りこちゃんが移動する
+const paintMapTile = (ctx, map, ts, tc, dy, dx, idx) => {
+    ctx.drawImage(
+        map,
+        ts * (idx % tc), ts * Math.floor(idx / tc), ts, ts,
+        ts * dx, ts * dy, ts, ts
+    );
+};
+
+const keyInput = (map) => {
     if (playerMove === 0) {
-        if (key.left === true) {
-            let x = playerX / TILESIZE;
-            let y = playerY / TILESIZE;
-            x--;
-            if (map[y][x] === 0) {
-                playerMove = TILESIZE;
-                key.push = 'left';
-            }
+        const { left, right, up, down } = key;
+        const x = playerX / map.TILESIZE;
+        const y = playerY / map.TILESIZE;
+
+        if (left && map.moveAllow.includes(map.map[y][x - 1])) {
+            playerMove = map.TILESIZE;
+            key.push = 'left';
         }
-        if (key.right === true) {
-            let x = playerX / TILESIZE;
-            let y = playerY / TILESIZE;
-            x++;
-            if (map[y][x] === 0) {
-                playerMove = TILESIZE;
-                key.push = 'right';
-            }
+        if (right && map.moveAllow.includes(map.map[y][x + 1])) {
+            playerMove = map.TILESIZE;
+            key.push = 'right';
         }
-        if (key.up === true) {
-            let x = playerX / TILESIZE;
-            let y = playerY / TILESIZE;
-            if (y > 0) {
-                y--;
-                if (map[y][x] === 0) {
-                    playerMove = TILESIZE;
-                    key.push = 'up';
-                }
-            }
+        if (up && y > 0 && map.moveAllow.includes(map.map[y - 1][x])) {
+            playerMove = map.TILESIZE;
+            key.push = 'up';
         }
-        if (key.down === true) {
-            let x = playerX / TILESIZE;
-            let y = playerY / TILESIZE;
-            if (y < MAP_WIDTH - 1) {
-                y++;
-                if (map[y][x] === 0) {
-                    playerMove = TILESIZE;
-                    key.push = 'down';
-                }
-            }
+        if (down && y < map.MAP_WIDTH - 1 && map.moveAllow.includes(map.map[y + 1][x])) {
+            playerMove = map.TILESIZE;
+            key.push = 'down';
         }
     }
 };
 
-const KeyOutput = () => {//rico.moveが0より大きい場合は、4pxずつ移動（いどう）を続ける
+const KeyOutput = (moveSpeed) => {
     if (playerMove > 0) {
-        playerMove -= 2;
-        if (key.push === 'left') playerX -= 2;
-        if (key.push === 'up') playerY -= 2;
-        if (key.push === 'right') playerX += 2;
-        if (key.push === 'down') playerY += 2;
+        playerMove -= moveSpeed;
+        if (key.push === 'left') playerX -= moveSpeed;
+        if (key.push === 'up') playerY -= moveSpeed;
+        if (key.push === 'right') playerX += moveSpeed;
+        if (key.push === 'down') playerY += moveSpeed;
     }
 };
 
